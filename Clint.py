44282bf291c357f00ff4f9d4e925a3e6ca1e1f6d
Samuel Ružek <samuel@samdo.sk>
2020-03-01 23:11:51 +0100

    websocket finaly works kinda

--- Clint.py
+++ Clint.py
@@ -3,53 +3,40 @@
 # WS client example
 
 import asyncio
-from websockets import client
+import websockets
 import json
 
-async with ws.websocket
 
-await def open(ws):
+async def open():
     uri = "wss://pubsub-edge.twitch.tv"
-    async ws.connect(uri)
+    async with websockets.connect(uri) as socket:
+        data = json.dumps({
+            "type":"LISTEN",
+            "data": {
+                "topics": ["channel-points-channel-v1.66504977"],
+                "auth_token": "46emmdbbto5srlmcvfy2tzxwwj6dij"
+            }
+        })
+        await socket.send(data)
+        resp = await socket.recv()
+        print(resp)
 
-await def recv(ws):
-    msg = await ws.recv()
 
-def test(ws):
-    ping = json.dumps({"type": "PING"})
-    ws.send(ping)
+async def test():
+    async with websockets.connect("wss://pubsub-edge.twitch.tv") as socket:
+        pinging = json.dumps({"type": "PING"})
+        await socket.send(pinging)
+        resp = await socket.recv()
+        print(resp)
 
 
-async get_event_loop().run_until_complete(test())
-
+asyncio.get_event_loop().run_until_complete(open())
+'''
+asyncio.get_event_loop().run_forever()
+'''
 
     
 
 
 
 
-
-
-
-
-'''
-async def hello():
-    uri = "wss://pubsub-edge.twitch.tv"
-    async with websockets.connect(uri) as websocket:
-        data = json.dumps({
-            "type":"LISTEN",
-            "data": {
-                "topics": ["channel-points-channel-v1.66504977"],
-                "auth_token": "kintzrnfggusiqqcc314ffjn9rralh"
-            }
-        })
-        await websocket.send(data)
-     
-
-        greeting = await websocket.recv()
-        print(greeting)
-
-asyncio.get_event_loop().run_until_complete(hello())
-
-asyncio.get_event_loop().run_forever(hello())
-'''
