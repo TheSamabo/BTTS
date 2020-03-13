a5b60b2ace26d026be897c51ee54952a1e8ca54e
Samuel Ružek <samuel@samdo.sk>
2020-03-14 00:22:36 +0100

    AUTH WORKS!!! next:GUI/APIreq

--- pyQttest.py
+++ pyQttest.py
@@ -1,29 +1,49 @@
 import sys
 from PyQt5.QtWidgets import *  
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWebKitWidgets import QWebView , QWebPage
-from PyQt5.QtWebKit import QWebSettings
+from PyQt5.QtCore import *
+from PyQt5.QtWebKitWidgets import *
+from PyQt5.QtWebKit import *
 from PyQt5.QtNetwork import *
 from auth_url import auth_request_url
 
+"""class deconst_url(url):
+    pass"""
+    
+
+class WebPage(QWebPage):
+    def userAgentForUrl(self, url):
+        return "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
 class Authorize(QWebView):
+
     def __init__(self):
+        
         self.view = QWebView.__init__(self)
         self.setWindowTitle("Auth")
         self.resize(600,400)
+        self.setPage(WebPage())
+        self.s1 = QScrollBar()
+        self.s1.setDisabled(True)
+       
     def load(self,url):
+        
         self.setUrl(QUrl(url))
+        
 
-
-    def user_auth(self):
+    def user_auth(self, url):
         self.showNormal()
         print("imworking")
-
+        self.urlChanged.connect(self.get_code)
+        
+# NEEDS MORE TESTING / DEBUGING
+    def get_code(self, url):
+        if authurl != url:
+            e = QUrlQuery(url)
+            value = e.queryItemValue("code")
+            print(value)
 
 
 if __name__ == "__main__":
- 
-    
+
     app = QApplication(sys.argv)
 
     w = QWidget()
@@ -31,13 +51,10 @@ if __name__ == "__main__":
     w.resize(300,300)
     w.setWindowTitle("igm")
     w.show()
-
-    label = QLabel(w)
-    label.setText("Ligma")
-    label.show()
-
     
     authurl = auth_request_url()
+
+
 
 
     auth = Authorize()
